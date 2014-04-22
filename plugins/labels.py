7740329ff9a9b49873046be96a387cd12265128e
Michael Wozniak <github@koh.ms>
2014-04-21 22:43:33 -0400

    ltc-> myr

--- plugins/labels.py
+++ plugins/labels.py
@@ -1,4 +1,4 @@
-from electrum_ltc.util import print_error
+from electrum_myr.util import print_error
 
 import httplib, urllib
 import socket
@@ -16,11 +16,11 @@ import PyQt4.QtCore as QtCore
 import PyQt4.QtGui as QtGui
 import aes
 import base64
-from electrum_ltc import bmp, pyqrnative
-from electrum_ltc.plugins import BasePlugin
-from electrum_ltc.i18n import _
+from electrum_myr import bmp, pyqrnative
+from electrum_myr.plugins import BasePlugin
+from electrum_myr.i18n import _
 
-from electrum_ltc_gui.qt import HelpButton, EnterButton
+from electrum_myr_gui.qt import HelpButton, EnterButton
 
 class Plugin(BasePlugin):
 
--- plugins/virtualkeyboard.py
+++ plugins/virtualkeyboard.py
@@ -1,6 +1,6 @@
 from PyQt4.QtGui import *
-from electrum_ltc import BasePlugin
-from electrum_ltc.i18n import _
+from electrum_myr import BasePlugin
+from electrum_myr.i18n import _
 
 class Plugin(BasePlugin):
 
