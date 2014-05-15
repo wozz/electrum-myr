80e1c026e1337be4f51ad9e450402af3f686110f
Michael Wozniak <github@koh.ms>
2014-05-14 21:42:51 -0400

    remove deps checking

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ data_files += [
 setup(
     name="Electrum-MYR",
     version=version.ELECTRUM_VERSION,
-    install_requires=['slowaes', 'ecdsa>=0.9', 'ltc_scrypt', 'groestl_hash', 'qubit_hash', 'skeinhash'],
+    install_requires=['slowaes', 'ecdsa>=0.9'],
     package_dir={
         'electrum_myr': 'lib',
         'electrum_myr_gui': 'gui',
