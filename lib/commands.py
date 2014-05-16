49badeb368e03bcc75310efcbf070a922b3da33d
Michael Wozniak <github@koh.ms>
2014-05-15 23:23:20 -0400

    multisig version numbers

--- lib/commands.py
+++ lib/commands.py
@@ -184,7 +184,7 @@ class Commands:
     def createmultisig(self, num, pubkeys):
         assert isinstance(pubkeys, list)
         redeem_script = Transaction.multisig_script(pubkeys, num)
-        address = hash_160_to_bc_address(hash_160(redeem_script.decode('hex')), 5)
+        address = hash_160_to_bc_address(hash_160(redeem_script.decode('hex')), 9)
         return {'address':address, 'redeemScript':redeem_script}
     
     def freeze(self,addr):
